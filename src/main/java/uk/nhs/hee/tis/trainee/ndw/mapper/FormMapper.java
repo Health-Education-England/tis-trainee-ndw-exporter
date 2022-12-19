ad741379f2a77945fc0a2d23eaf6b8ce09aa84f5
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2022-12-19 15:41:57 +0000

    fix: remove unneeded mapper

--- src/main/java/uk/nhs/hee/tis/trainee/ndw/mapper/FormMapper.java
+++
@@ -1,8 +0,0 @@
-package uk.nhs.hee.tis.trainee.ndw.mapper;
-
-import org.mapstruct.Mapper;
-import uk.nhs.hee.tis.trainee.ndw.mapper.util.FormUtil;
-
-@Mapper(componentModel = "spring", uses = FormUtil.class)
-public interface FormMapper {
-}
